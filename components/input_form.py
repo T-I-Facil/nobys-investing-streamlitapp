07d10af0c296af012965ea255ea9942e9ff42a8f
shadyrajaab <shadyrajaab@gmail.com>
2024-10-31 17:33:00 -0300

    style: rename form

--- components/input_form.py
+++ components/input_form.py
@@ -3,11 +3,12 @@ import streamlit as st
 
 def get_input_form():
     FORMULARIO = {
-        "nome":  st.text_input("sua mãe?"),
-        "valor_inicial_nota": st.number_input("nota fiscal?"),
-        "nota_fiscal": st.text_input("nota fiscal?"),
-        "data_operacao": st.date_input("data da operação"),
-        "valor_emprestado": st.number_input("valor emprestado"),
-        "dias_adiantados": st.number_input("dias adiantados")
+        "nome":  st.text_input("Cliente"),
+        "valor_inicial_nota": st.number_input("Valor da Nota"),
+        "nota_fiscal": st.text_input("Nº Nota Fiscal"),
+        "data_operacao": st.date_input("Data da Operação", format="DD/MM/YYYY"),
+        "valor_emprestado": st.number_input("Valor Emprestado"),
+        "dias_adiantados": st.number_input("Dias Adiantados", format="%.0f")
     }
+
     return FORMULARIO
