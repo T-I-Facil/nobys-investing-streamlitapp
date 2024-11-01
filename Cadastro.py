213406e3bfb1c854da3b5160ece82c72d9fbf2e8
ThallesHSilva <thallesths97@gmail.com>
2024-11-01 13:31:23 -0300

    Correções

--- Cadastro.py
+++ Cadastro.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Nobys", layout="centered", page_icon="assets/noby
 style_metric_cards(border_radius_px=20)
 
 st.sidebar.image("assets/nobys_banner.png")
-juros_simulado = (9.8/30)*7
+juros_simulado = (0.098/30)*7
 c1, c2 = st.columns(2)
 m1, m2, m3 = st.columns(3)
 
@@ -18,10 +18,11 @@ valor_nota_simulado = c1.number_input("Valor da Nota", step=0.5)
 dias_adiantados_simulado = c2.number_input("Dias Adiantados", step=1)
 
 if dias_adiantados_simulado > 7:
-    juros_simulado = (9.8/30)*dias_adiantados_simulado
+    juros_simulado = (0.098/30)*dias_adiantados_simulado
 
 juros_final = juros_simulado*valor_nota_simulado
-m1.metric(label="Juros", value=f"{juros_simulado:,.2f}%".replace(".", ","))
+juros_string = juros_simulado*100
+m1.metric(label="Juros", value=f"{juros_string:,.2f}%".replace(".", ","))
 m2.metric(label="Valor Final:", value=f"R$ {valor_nota_simulado-juros_final:,.2f}".replace(".", ","))
 m3.metric(label="Valor Juros", value=f"R$ {juros_final:,.2f}".replace(".", ","))
 
@@ -32,9 +33,9 @@ with st.form("Formulário"):
     dias_adiantados = input_data['dias_adiantados']
     valor_nota = input_data['valor_inicial_nota']
 
-    juros = (9.8/30)*7
+    juros = (0.098/30)*7
     if dias_adiantados > 7:
-        juros = (9.8/30)*dias_adiantados
+        juros = (0.098/30)*dias_adiantados
 
     if submit:
         input_data_atualizado = {**input_data, "juros": juros}
--- database/db_handler.py
+++ database/db_handler.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 from .models.input_data import InputData
 from datetime import datetime
 
